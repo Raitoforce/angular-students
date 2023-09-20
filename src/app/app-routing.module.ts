2dfee622c8f40b013d820b32f5ac5e1ba087a2ae
Raitoforce <mahgt_34@hotmail.com>
2023-09-20 08:14:10 -0600

    ✨add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StudentListComponent} from "./students/student-list/student-list.component";
+import {StudentFormComponent} from "./students/student-form/student-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: StudentListComponent },
+  { path: 'form', component: StudentFormComponent },
+  { path: 'form/:id', component: StudentFormComponent },
+  { path: '**', redirectTo: 'list' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
